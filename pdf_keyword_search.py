9c0b54df063b767f8192ec005c50b8c0d9f9df2d
joaoppadua <joaopadua@id.uff.br>
2023-11-30 17:10:06 -0300

    adde pdf_keyword_search file

---
+++ pdf_keyword_search.py
@@ -0,0 +1,39 @@
+#Script to find pdf files with keywords in them
+import os
+import sys
+import PyPDF2
+
+def find_keyword_in_pdfs(folder_path, keyword):
+    matching_files = []
+
+    for filename in os.listdir(folder_path):
+        if filename.endswith('.pdf'):
+            try:
+                file_path = os.path.join(folder_path, filename)
+                with open(file_path, 'rb') as file:
+                    pdf_reader = PyPDF2.PdfReader(file)
+                    num_pages = len(pdf_reader.pages)
+
+                    for page_num in range(num_pages):
+                        page = pdf_reader.pages[page_num]
+                        text = page.extract_text()
+                        if keyword in text:
+                            matching_files.append(filename)
+                            break  # Stop searching this file as soon as the keyword is found
+            except Exception as e:
+                print(f"Error reading {filename}: {e}")
+
+    return matching_files
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: python pdf_keyword_search.py [folder_path] [keyword]")
+        sys.exit(1)
+
+    folder_path = sys.argv[1]
+    keyword = sys.argv[2]
+    matching_files = find_keyword_in_pdfs(folder_path, keyword)
+
+    print("Files containing the keyword:")
+    for file in matching_files:
+        print(file)
